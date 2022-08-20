c14cc4bf0a9c1ff5cfd44a675c56261dff3446c3
Jaydon <pearcejaydon@gmail.com>
2022-08-20 17:52:04 +1000

    Add new todo form and change to scss

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const addTaskButton = document.getElementById('addTaskBtn');
+const todoWrapper = document.querySelector('.project-todo-wrapper');
+
+
+addTaskButton.addEventListener('click', ()=>{
+    
+    addTaskButton.classList.add('active');
+    
+});
