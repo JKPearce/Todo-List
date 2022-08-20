77aa5872a2a9067652e246b05666f03dd6baa35b
Jaydon <pearcejaydon@gmail.com>
2022-08-20 22:55:24 +1000

    Add style and functionality to button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import "./scss/main.scss";
 
 const addTaskButton = document.getElementById('addTaskBtn');
-const todoWrapper = document.querySelector('.project-todo-wrapper');
+const addTaskForm = document.getElementById('newTaskForm');
 
+const toggleTaskForm = (toggle) => {
+    if (toggle) {
+        addTaskButton.classList.add('active');
+        addTaskForm.classList.add('active');
+    } else {
+        addTaskButton.classList.remove('active');
+        addTaskForm.classList.remove('active');
+    }
+}
 
-addTaskButton.addEventListener('click', ()=>{
-    
-    addTaskButton.classList.add('active');
-    
+addTaskButton.addEventListener('click', () => {
+    addTaskForm.reset();
+    toggleTaskForm(true);
 });
+
+const saveTaskBtn = document.getElementById('submitNewTask');
+const cancelNewTask = document.getElementById('cancelNewTask');
+
+saveTaskBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    //save user data to a new task class from import
+
+    toggleTaskForm(false);
+});
+
+cancelNewTask.addEventListener('click', toggleTaskForm(false));
+
+
