4bf495a3f76d1d5bd20823fe7da320c023ef5e12
Jaydon <pearcejaydon@gmail.com>
2022-08-20 18:20:26 +1000

    Format scss file

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,10 +1,3 @@
-class Todo {
-    constructor(title, description, dueDate, priority){
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
-
+class TodoList {
     
 }
